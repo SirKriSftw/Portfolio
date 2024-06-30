32491849dee87d11f702766bc844028d4e0174fe
Kristian Perez <SirKriSftw@hotmail.com>
2024-06-30 13:00:31 -0400

    Added a commands service and command model. Later when I hook this all up to a DB ill just have to update the service files since the components will already be using the service functions. Also made commands component that displays all commands.

---
+++ PortfolioNG/src/app/services/commands.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { Command } from '../models/command.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommandsService {
+
+  constructor() { }
+
+  getAllCommands() : Observable<Command[]>
+  {
+    return from(new Promise<Command[]>((resolve, reject) => {
+      const commands = [
+        {
+          name: "help",
+          description: "To use help command just do 'help <command name>, command list can be found using commands command.'"
+        },
+        {
+          name: "commands",
+          description: "Shows list of all available commands."
+        },
+        {
+          name: "about-me",
+          description: "More about me, Kristian Perez."
+        },
+        {
+          name: "contact-me",
+          description: "Information on how to contact me, Kristian Perez, include linkedIn, email and number."
+        },
+        {
+          name: "projects",
+          description: "A list of my past projects."
+        },
+        {
+          name: "skills",
+          description: "A list of technologies I am comfortable using."
+        },
+        {
+          name: "testimonials",
+          description: "What past co-workers have to say about me."
+        }
+      ];
+      resolve(commands);
+    }));
+  }
+
+  getAllCommandNames() : Observable<string[]>
+  {
+    return from(
+      new Promise<string[]>((resolve, reject) => {
+        const commandNames = ["help", "commands", "about-me","contact-me","projects","skills","testimonials"];
+        resolve(commandNames);
+      })
+    )
+  }
+}
