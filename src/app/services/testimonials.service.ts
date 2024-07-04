90fbfb1868c5650f3787c3a2e62d9322ddc05388
Kristian Perez <SirKriSftw@hotmail.com>
2024-07-03 20:12:55 -0400

    Added new testimonial and re-built

--- src/app/services/testimonials.service.ts
+++ src/app/services/testimonials.service.ts
@@ -21,8 +21,21 @@ export class TestimonialsService {
           "I also had the privilege of working with him on the same team on a group project.  In addition to the software development technical expertise that he contributed to our team, he also demonstrated that he was able to cooperate well with the rest of the team for the greater goal of the whole project. " +
           "He was able to do this due to his ability to communicate clearly, kindly, and patiently with others.\n" +
           "Although I have not worked with him since 2022, I would be happy to have him on my team again any point in the future. " + 
-          "I am glad to have the chance to write this testimonial for him so that I can vouch for his ability to be an integral contributor to any software development team he ends up on.",
+          "I am glad to have the chance to write this testimonial for him so that I can vouch for his ability to be an integral contributor to any software development team he ends up on.\n " +
+          "Feel free to reach me at my personal email address; we can also connect over the telephone upon request.",
           contact: "thecodercody@gmail.com"
+        },
+        {
+          name: "Michael Lai",
+          relationship: "Ex-Revature Coworker",
+          message: "We worked closely together at Revature where he was group leader for one of the projects while I was working on the API code.\n " +
+          "Throughout our time working together, I got to know Kristian well on both a professional and personal level. He is trustworthy, responsible, hard-working, and loyal — traits that make them an exceptional colleague and team player.\n " +
+          "Speaking on his skill, his understanding of how Git Hub works has helped our team in organizing our projects in terms on having necessary branches and builds so people’s work was not overwritten when we sync our code together." + 
+          "He works well with other and has often assist the team members in resolving problematic area of the code.\n " + 
+          "Together, we were able to complete the final project, which would not have been possible without Kristian’s commitment, resilience, and creative problem-solving skills.\n " +
+          "I wholeheartedly believe Kristian has the skills, experience, and spirit to tackle anything required of them. They are an asset to any employer, and you can trust them to succeed in the position and exceed your exceptions.\n " +
+          "If you have any questions, please feel free to contact me.",
+          contact: "Michael.C.Lai@outlook.com"
         },
       ];
       resolve(testimonials);
